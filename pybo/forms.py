27effc1852b53d0fba5d9f290a686b848e819bb0
정후영 <wjdgndud9470@naver.com>
2022-03-27 15:37:18 +0900

    파이보 프로젝트 최초 커밋

---
+++ pybo/forms.py
@@ -0,0 +1,23 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, TextAreaField, PasswordField, EmailField
+from wtforms.validators import DataRequired, Length, EqualTo, Email
+
+class QuestionForm(FlaskForm):
+    subject = StringField('제목', validators=[DataRequired('제목은 필수입력 항목입니다.')])
+    content = TextAreaField('내용', validators=[DataRequired('내용은 필수입력 항목입니다.')])
+
+class AnswerForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired('내용은 필수입력 항목입니다.')])
+
+class UserCreateForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired('이름은 필수입력 항목입니다.'), Length(min=3, max=25)])
+    password1 = PasswordField('비밀번호', validators=[DataRequired('비밀번호는 필수입력 항목입니다.'), EqualTo('password2', '비밀번호가 일치하지 않습니다.')])
+    password2 = PasswordField('비밀번호확인', validators=[DataRequired('')])
+    email = EmailField('이메일', validators=[DataRequired('이메일은 필수입력 항목입니다.'), Email()])
+
+class UserLoginForm(FlaskForm):
+    username = StringField('사용자이름', validators=[DataRequired("이름은 필수입력 항목입니다."), Length(min=3, max=25)])
+    password = PasswordField('비밀번호', validators=[DataRequired("비밀번호는 필수입력 항목입니다.")])
+
+class CommentForm(FlaskForm):
+    content = TextAreaField('내용', validators=[DataRequired()])
